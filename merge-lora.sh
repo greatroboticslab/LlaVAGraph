1cfd24fc6783366e1af85899654aa4dee2e73fde
IMOsbo <imosborne@outlook.com>
2024-12-12 14:39:01 -0600

    Added slurm files

---
+++ merge-lora.sh
@@ -0,0 +1,2 @@
+python scripts/merge_lora_weights.py --model-path /data/isaiah/checkpoints/llava-v1.5-13b-task-lora --model-base /data/isaiah/merged-llava-lora --save-model-path /data/isaiah/LLaVAChartv2
+
