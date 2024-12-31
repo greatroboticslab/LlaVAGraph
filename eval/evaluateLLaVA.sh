9254a719fe3fe01213d89774eb0c7dffc43b12ec
IMOsbo <imosborne@outlook.com>
2024-12-31 11:46:28 -0600

    Finished categorization pipeline

--- eval/evaluateLLaVA.sh
+++ eval/evaluateLLaVA.sh
@@ -6,10 +6,10 @@ IMAGEFOLDER=/home/imo2d/LLaVA/data/subset/trainData
 SUBSET=10
 
 # random noise
-python evaluateLLaVA.py --model-path $MODELPATH --image-folder $IMAGEFOLDER/NoiseData --output-file results/randomNoise.json --subset $SUBSET
+python evaluateLLaVA.py --model-path $MODELPATH --image-folder $IMAGEFOLDER/NoiseData --output-file results/llava/randomNoise.json --subset $SUBSET
 
 # sine waves
-python evaluateLLaVA.py --model-path $MODELPATH --image-folder $IMAGEFOLDER/SineData --output-file results/sineWave.json --subset $SUBSET
+python evaluateLLaVA.py --model-path $MODELPATH --image-folder $IMAGEFOLDER/SineData --output-file results/llava/sineWave.json --subset $SUBSET
 
 #square waves
-python evaluateLLaVA.py --model-path $MODELPATH --image-folder $IMAGEFOLDER/SquareData --output-file results/squareWave.json --subset $SUBSET
+python evaluateLLaVA.py --model-path $MODELPATH --image-folder $IMAGEFOLDER/SquareData --output-file results/llava/squareWave.json --subset $SUBSET
